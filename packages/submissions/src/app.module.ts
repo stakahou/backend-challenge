720dbfc911e044dd4e12ed53195380f01fac34f6
Joshua <joshua.alves@dellead.com>
2021-09-28 21:45:44 -0300

    Added TypeOrm | Database connection.

--- packages/submissions/src/app.module.ts
+++ packages/submissions/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import graphqlConfig from './configs/graphql.config';
+import ormConfig from './configs/orm.config';
 import { SubmissionModule } from './modules/submission/submission.module';
-
 @Module({
-  imports: [GraphQLModule.forRoot(graphqlConfig), SubmissionModule],
+  imports: [
+    GraphQLModule.forRoot(graphqlConfig),
+    TypeOrmModule.forRoot(ormConfig),
+    SubmissionModule,
+  ],
 })
 export class AppModule {}
