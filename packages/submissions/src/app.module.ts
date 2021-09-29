098802790ad95c26c388a32b10c2776f055b6b22
Joshua <joshua.alves@dellead.com>
2021-09-28 22:56:44 -0300

    Revert "Create Back End challenge"
    This reverts commit f2200f380dc190e7ccb2ee86ee5ea9561296adfe.

--- packages/submissions/src/app.module.ts
+++ packages/submissions/src/app.module.ts
@@ -3,8 +3,9 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import graphqlConfig from './configs/graphql.config';
 import ormConfig from './configs/orm.config';
-import { SubmissionModule } from './modules/submission/submission.module';
 import { ChallengeModule } from './modules/challenge/challenge.module';
+import { SubmissionModule } from './modules/submission/submission.module';
+
 @Module({
   imports: [
     GraphQLModule.forRoot(graphqlConfig),
